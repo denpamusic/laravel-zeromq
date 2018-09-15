a5517ea5daa53d518fed317b6cd2ca37f3a63690
denpamusic <denpa@netfleet.space>
2018-09-16 01:02:26 +0300

    Added config var descriptions.

--- config/config.php
+++ config/config.php
@@ -1,11 +1,41 @@
 <?php
 
 return [
+
     'connections' => [
+
         'default' => [
+            /*
+            |--------------------------------------------------------------------------
+            | ZeroMQ protocol
+            |--------------------------------------------------------------------------
+            | Transport protocol that'll be used to
+            | establish ZeroMQ connections.
+            |
+            */
+
             'protocol' => 'tcp',
-            'host'     => '127.0.0.1',
-            'port'     => 28332,
+
+            /*
+            |--------------------------------------------------------------------------
+            | ZeroMQ host
+            |--------------------------------------------------------------------------
+            | IP or hostname of peer with which ZeroMQ connection
+            | should be established.
+            |
+            */
+
+            'host' => 'localhost',
+
+            /*
+            |--------------------------------------------------------------------------
+            | ZeroMQ port
+            |--------------------------------------------------------------------------
+            | Port to which ZeroMQ connection should
+            | be established.
+            */
+
+            'port' => 28332,
         ],
     ],
 ];
